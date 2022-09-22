2a654b36cd0919339dcf2e5e24e1e51392a13bbd
Eugenia <evg_vit2015@gmail.com>
2022-09-22 16:11:16 +0500

    задача 8 дз

---
+++ Семинар 1/Домашнее задание/задача 8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Number= ");
+int number = int.Parse(Console.ReadLine());
+int i =1;
+if (number%2==0){
+while (i <(number-1)){
+    if (i%2==0) Console.Write($"{i}, ");
+    i++;
+}
+Console.Write(number);}
+else{
+    while (i <(number-1)){
+    if (i%2==0) Console.Write($"{i}, ");
+    i++;
+}
+Console.Write(number-1);}
