8f9eaed1c0c3f592ebfeb87b579ebe1b5848c290
Eugenia <evg_vit2015@gmail.com>
2022-09-22 15:46:37 +0500

    задача 2 дз

---
+++ Семинар 1/Домашнее задание/задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("First number= ");
+int numberFirst = int.Parse(Console.ReadLine());
+Console.Write("Second number = ");
+int numberSecond = int.Parse(Console.ReadLine());
+if (numberSecond==numberFirst){
+    Console.WriteLine("numbers are equal ");
+}
+else {
+    if(numberFirst>numberSecond)
+    {
+        Console.WriteLine($"max={numberFirst}");
+        Console.WriteLine($"min={numberSecond}");
+    }
+    else{
+        Console.WriteLine($"max={numberSecond}");
+        Console.WriteLine($"min={numberFirst}");
+        
+    }
+}
