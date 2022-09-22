1d50efc1319c9c3511830016f8c52ae0008da7b0
Eugenia <evg_vit2015@gmail.com>
2022-09-22 15:54:41 +0500

    задача 4 дз

---
+++ Семинар 1/Домашнее задание/задача 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("First number= ");
+int numberFirst = int.Parse(Console.ReadLine());
+Console.Write("Second number = ");
+int numberSecond = int.Parse(Console.ReadLine());
+Console.Write("Third number = ");
+int numberThird = int.Parse(Console.ReadLine());
+int max = numberFirst;
+if (numberSecond>max) max=numberSecond;
+if(numberThird>max) max=numberThird;
+
+Console.WriteLine($"max= {max}");
