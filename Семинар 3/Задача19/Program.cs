00999dbcf082ef03fd814d2047941d071271a8d0
Eugenia <evg_vit2015@gmail.com>
2022-09-29 01:15:23 +0500

    семинар 3 домашка

---
+++ Семинар 3/Задача19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if ((Math.Abs(number)>99999) || (Math.Abs(number)<9999))
+{
+    Console.WriteLine(" not correct");
+}
+else
+{
+if ( number/10000 == number%10 && number/1000%10==number%100/10)
+{
+    Console.WriteLine($"{number} -> yes");
+}
+else 
+{
+        Console.WriteLine($"{number} -> no");
+     //   Console.WriteLine($"{number/10000},{number%10},{number/1000%10},{number%100/10}");   // проверка правильности формул
+
+}
+}
---
+++ Семинар 3/Задача21/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+
+Console.Write("Enter XA: ");
+int x0 = int.Parse(Console.ReadLine());
+Console.Write("Enter YA: ");
+int y0 = int.Parse(Console.ReadLine());
+Console.Write("Enter ZA: ");
+int z0 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter XB: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Enter YB: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Enter ZB: ");
+int z1 = int.Parse(Console.ReadLine());
+
+double s = Math.Sqrt((x1-x0)*(x1-x0)+(y1-y0)*(y1-y0)+(z1-z0)*(z1-z0));
+
+Console.WriteLine($"A({x0},{y0},{z0}); B({x1},{y1},{z1}) -> {Math.Round(s,2)}");
